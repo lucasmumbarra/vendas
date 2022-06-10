c39a3cabac095f36b909e5f358f5d0bdbdc15521
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-06-10 10:47:55 -0300

    Requisição GET com parâmetros e Response Entity

--- src/main/java/io/github/lucasmumbarra/vendas/VendasApplication.java
+++ src/main/java/io/github/lucasmumbarra/vendas/VendasApplication.java
@@ -1,10 +1,24 @@
 package io.github.lucasmumbarra.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.github.lucasmumbarra.vendas.domain.entity.Cliente;
+import io.github.lucasmumbarra.vendas.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
+
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes) {
+		return args -> {
+			Cliente c = new Cliente(null, "Fulano");
+			clientes.save(c);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
