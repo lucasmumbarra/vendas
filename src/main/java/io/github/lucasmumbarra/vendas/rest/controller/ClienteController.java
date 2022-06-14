0a63bdf1d7c1e524da30b7ef7c0b3c310aa23261
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-06-14 15:16:43 -0300

    Pesquisa de Clientes por parâmetros

--- src/main/java/io/github/lucasmumbarra/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/github/lucasmumbarra/vendas/rest/controller/ClienteController.java
@@ -1,7 +1,10 @@
 package io.github.lucasmumbarra.vendas.rest.controller;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -64,5 +67,17 @@ public class ClienteController {
                 clientes.save(cliente);
                 return ResponseEntity.noContent().build();
               } ).orElseGet( () -> ResponseEntity.notFound().build() );
+  }
+
+  @GetMapping("/api/clientes")
+  public ResponseEntity find( Cliente filtro ) {
+    ExampleMatcher matcher = ExampleMatcher
+                                  .matching()
+                                  .withIgnoreCase()
+                                  .withStringMatcher( ExampleMatcher.StringMatcher.CONTAINING );
+
+    Example example = Example.of(filtro, matcher);
+    List<Cliente> lista = clientes.findAll(example);
+    return ResponseEntity.ok(lista);
   }
 }
