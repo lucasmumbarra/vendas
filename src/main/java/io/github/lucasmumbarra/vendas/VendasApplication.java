25a80b00070ed5d16417f42e4baffde5f17a2753
Lucas Mumbarra <lucassantosm.2014@gmail.com>
2022-06-14 14:28:36 -0300

    Delete Mapping: deletando um recurso no servidor

--- src/main/java/io/github/lucasmumbarra/vendas/VendasApplication.java
+++ src/main/java/io/github/lucasmumbarra/vendas/VendasApplication.java
@@ -1,13 +1,7 @@
 package io.github.lucasmumbarra.vendas;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import io.github.lucasmumbarra.vendas.domain.entity.Cliente;
-import io.github.lucasmumbarra.vendas.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
--- src/main/java/io/github/lucasmumbarra/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/github/lucasmumbarra/vendas/rest/controller/ClienteController.java
@@ -4,12 +4,11 @@ import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.github.lucasmumbarra.vendas.domain.entity.Cliente;
@@ -26,7 +25,7 @@ public class ClienteController {
 
   @GetMapping("/api/clientes/{id}")
   @ResponseBody
-  public ResponseEntity getClienteById(@PathVariable Integer id) {
+  public ResponseEntity getClienteById( @PathVariable Integer id ) {
     Optional<Cliente> cliente = clientes.findById(id);
     if (cliente.isPresent()) {
       return ResponseEntity.ok(cliente.get());
@@ -41,5 +40,16 @@ public class ClienteController {
     Cliente clienteSalvo = clientes.save(cliente);
     return ResponseEntity.ok(clienteSalvo);
   }
-  
+
+  @DeleteMapping("/api/clientes/{id}")
+  @ResponseBody
+  public ResponseEntity delete( @PathVariable Integer id ) {
+    Optional<Cliente> cliente = clientes.findById(id);
+    if (cliente.isPresent()) {
+      clientes.delete(cliente.get() );
+      return ResponseEntity.noContent().build();
+    }
+
+    return ResponseEntity.notFound().build();
+  }
 }
